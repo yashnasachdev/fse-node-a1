994c6e02f39e8d91502569eb75bf60ccb58e235e
yashna Sachdev <yashna.sach@gmail.com>
2022-02-27 03:50:33 -0500

    added tuit controller

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import UserDao from './dao/UserDao';
-
+import TuitController from "./controllers/TuitController";
+import TuitDao from './dao/TuitDao';
 import mongoose from "mongoose";
 mongoose.connect('mongodb://127.0.0.1:27017/tuiter').then(() => {
     console.log("Connected to DB");
@@ -16,8 +17,13 @@ app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
 var userDao: UserDao;
+var tuitDao: TuitDao;
+
 userDao = new UserDao();
 const userController = new UserController(app, userDao);
+
+tuitDao = new TuitDao();
+const tuitController = new TuitController(app, tuitDao);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
