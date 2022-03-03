e84d155661a6ccfa38269f8bad6e3fc57ffdea71
yashna Sachdev <yashna.sach@gmail.com>
2022-03-03 18:03:48 -0500

    schema

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose"
 
 const TuitSchema = new mongoose.Schema({
     tuit: String,
     postedOn: Date,
-    postedBy: {username: {type: String, required: true},
-             password: {type: String, required: true},
-             firstName: String,
-             lastName: String,
-             email: String,
-             profilePhoto: String,
-             headerImage: String,
-             accountType: {type: String, default: 'PERSONAL', enum: ['PERSONAL', 'ACADEMIC', 'PROFESSIONAL']},
-             maritalStatus: {type: String, default: 'SINGLE', enum: ['MARRIED', 'SINGLE', 'WIDOWED']},
-             biography: String,
-             dateOfBirth: Date,
-             joined: {type: Date, default: Date.now},
-             location: {
-              latitude: {type: Number, default: 0.0},
-              longitude: {type: Number, default: 0.0},
-             }}
+    postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"}
+//              password: {type: String, required: true},
+//              firstName: String,
+//              lastName: String,
+//              email: String,
+//              profilePhoto: String,
+//              headerImage: String,
+//              accountType: {type: String, default: 'PERSONAL', enum: ['PERSONAL', 'ACADEMIC', 'PROFESSIONAL']},
+//              maritalStatus: {type: String, default: 'SINGLE', enum: ['MARRIED', 'SINGLE', 'WIDOWED']},
+//              biography: String,
+//              dateOfBirth: Date,
+//              joined: {type: Date, default: Date.now},
+//              location: {
+//               latitude: {type: Number, default: 0.0},
+//               longitude: {type: Number, default: 0.0},
+//              }}
 }, {collection: "tuits"})
 export default TuitSchema;
