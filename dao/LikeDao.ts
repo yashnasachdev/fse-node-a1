b65f2af2e6784a3a7dc84421540c2dc90bb7d019
yashnasachdev <yashna.sach@gmail.com>
2022-04-09 20:03:53 -0400

    mongodb atlas connection

--- dao/LikeDao.ts
+++ dao/LikeDao.ts
@@ -1,6 +1,17 @@
 import LikeDaoI from "../interfaces/LikeDaoI";
 import LikeModel from "../mongoose/LikeModel";
 import Like from "../models/Like";
+
+
+/**
+ * @file Implements DAO managing data storage of Likes. Uses mongoose LikeModel
+ * to integrate with MongoDB
+ */
+/**
+ * @class LikeDao Implements Data Access Object managing data storage
+ * of Likes
+ * @property {LikeDao} likeDao Private single instance of LikeDao
+ */
 export default class LikeDao implements LikeDaoI {
     private static likeDao: LikeDao | null = null;
     public static getInstance = (): LikeDao => {
@@ -10,18 +21,40 @@ export default class LikeDao implements LikeDaoI {
         return LikeDao.likeDao;
     }
     private constructor() {}
+    /**
+    * Uses LikeModel to retrieve all like documents from likes collection
+    * @returns Promise To be notified when the likes are retrieved from
+    * database
+    */
     findAllUsersThatLikedTuit = async (tid: string): Promise<Like[]> =>
         LikeModel
             .find({tuit: tid})
             .populate("likedBy")
             .exec();
+    /**
+    * Uses LikeModel to retrieve all like documents from likes collection
+    * @returns Promise To be notified when the likes are retrieved from
+    * database
+    */
     findAllTuitsLikedByUser = async (uid: string): Promise<Like[]> =>
         LikeModel
             .find({likedBy: uid})
             .populate("tuit")
             .exec();
+    /**
+    * Inserts like instance into the database
+    * @param {string} uid Primary key of user liking the tuit
+    * @param {string} tid Primary key of tuit being liked
+    * @returns Promise To be notified when like is inserted into the database
+    */
     userLikesTuit = async (uid: string, tid: string): Promise<any> =>
         LikeModel.create({tuit: tid, likedBy: uid});
+    /**
+    * Removes like from the database.
+    * @param {string} uid Primary key of user unliking the tuit
+    * @param {string} tid Primary key of tuit being unliked
+    * @returns Promise To be notified when like is removed from the database
+    */
     userUnlikesTuit = async (uid: string, tid: string): Promise<any> =>
         LikeModel.deleteOne({tuit: tid, likedBy: uid});
 };
--- server.ts
+++ server.ts
@@ -53,15 +53,15 @@ import MessageController from "./controllers/MessageController"
 import mongoose from "mongoose";
 
 // build the connection string
-const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// const PROTOCOL = "mongodb+srv";
+ const DB_USERNAME = process.env.DB_USERNAME;
+ const DB_PASSWORD = process.env.DB_PASSWORD;
+ const HOST = "cluster0.m8jeh.mongodb.net";
+// const DB_NAME = "myFirstDatabase";
+// const DB_QUERY = "retryWrites=true&w=majority";
+// const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
-mongoose.connect(connectionString);
+mongoose.connect('mongodb+srv://yashna:sachdev@cluster0.p3chr.mongodb.net/test?retryWrites=true&w=majority');
 
 const app = express();
 app.use(express.json());
