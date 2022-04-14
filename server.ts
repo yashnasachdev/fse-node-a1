e39ebdae2e59375ef5d69c9cf9a6d95f95625a47
yashnasachdev <yashna.sach@gmail.com>
2022-04-13 22:15:11 -0400

    mongodb connection edit

--- server.ts
+++ server.ts
@@ -1,35 +1,3 @@
-/*import express, {Request, Response} from 'express';
-import UserController from "./controllers/UserController";
-import UserDao from './dao/UserDao';
-import TuitController from "./controllers/TuitController";
-import TuitDao from './dao/TuitDao';
-import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter').then(() => {
-    console.log("Connected to DB");
-  });;
-
-const app = express();
-app.use(express.json());
-
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
-
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
-
-var userDao: UserDao;
-var tuitDao: TuitDao;
-
-userDao = new UserDao();
-const userController = new UserController(app, userDao);
-
-tuitDao = new TuitDao();
-const tuitController = new TuitController(app, tuitDao);
-
-const PORT = 4000;
-app.listen(process.env.PORT || PORT);
-*/
-
 /**
  * @file Implements an Express Node HTTP server. Declares RESTful Web services
  * enabling CRUD operations on the following resources:
@@ -37,6 +5,9 @@ app.listen(process.env.PORT || PORT);
  *     <li>users</li>
  *     <li>tuits</li>
  *     <li>likes</li>
+ *     <li>follows</li>
+ *     <li>bookmarks</li>
+ *     <li>messages</li>
  * </ul>
  *
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
@@ -52,15 +23,24 @@ import MessageController from "./controllers/MessageController"
 
 import mongoose from "mongoose";
 
-// build the connection string
+const dotenv = require("dotenv")
+dotenv.config()
+
+//build the connection string
 // const PROTOCOL = "mongodb+srv";
- const DB_USERNAME = process.env.DB_USERNAME;
- const DB_PASSWORD = process.env.DB_PASSWORD;
- const HOST = "cluster0.m8jeh.mongodb.net";
+// const DB_USERNAME = process.env.DB_USERNAME;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+// const HOST = "cluster0.mc9fo.mongodb.net";
 // const DB_NAME = "myFirstDatabase";
 // const DB_QUERY = "retryWrites=true&w=majority";
 // const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// connect to the database
+
+//mongoose.connect('mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD
+//      + '@cluster0.mc9fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+// mongoose.connect(connectionString)
+//connect to the database
+//mongoose.connect('mongodb+srv://DB_USERNAME:DB_PASSWORD@cluster0.mc9fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 mongoose.connect('mongodb+srv://yashna:sachdev@cluster0.mc9fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 const app = express();
