8bd8f4ea98384ffa462d8188aa2998682b7322f1
yashnasachdev <yashna.sach@gmail.com>
2022-04-23 04:09:29 -0400

    tuit schema update

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import Tuit from "../models/Tuit";
 const TuitSchema = new mongoose.Schema<Tuit>({
-    tuit: {type: String, required: true},
+    tuit: {type: String},
     postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
     postedOn: {type: Date, default: Date.now}
 }, {collection: "tuits"});
